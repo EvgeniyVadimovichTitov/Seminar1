267ab847784e7c0d143d6cca628ec20f2277df07
EvgeniyVadimovichTitov <jekon777999@gmail.com>
2022-08-29 18:46:40 +0300

    Complete task3 seminar2

--- seminar2/Program.cs
+++ seminar2/Program.cs
@@ -1,7 +1,9 @@
-﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-int number = new Random().Next(100, 1000);
-Console.WriteLine($"Random number = {number}");
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+int number = new Random().Next(1, 8);
+Console.WriteLine($"Random week's day = {number}");
 
-string newformatnumber = number.ToString();
+int[] DayWeek = { 1, 2, 3, 4, 5, 6, 7 };
 
-Console.WriteLine(newformatnumber[1]);
+if (number == DayWeek[5] || number == DayWeek[6]) Console.WriteLine("Yes");
+else Console.WriteLine("No");
+
