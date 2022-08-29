862b564fd551d421a7e6b6fa4c94a6b0542372bd
EvgeniyVadimovichTitov <jekon777999@gmail.com>
2022-08-29 17:31:02 +0300

    Complete task1

---
+++ seminar2/Program.cs
@@ -0,0 +1,7 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Random number = {number}");
+
+string reductnumber = number.ToString();
+
+Console.WriteLine(reductnumber[1]);
