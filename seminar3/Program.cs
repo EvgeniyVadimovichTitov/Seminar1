290a6627fdff220a06885de6971dc68ecad6fb60
EvgeniyVadimovichTitov <jekon777999@gmail.com>
2022-08-30 15:23:02 +0300

    Complete Task2

--- seminar3/Program.cs
+++ seminar3/Program.cs
@@ -1,4 +1,26 @@
-﻿// Seminar3Task1 - Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
-// Seminar3Task2 - Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
-// Seminar3Task3 - Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+﻿// Seminar3Task2 - Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+Console.WriteLine("Введите Х1: ");
+int xa = 0;
+int.TryParse(Console.ReadLine(), out xa);
 
+Console.WriteLine("Введите Y1: ");
+int ya = 0;
+int.TryParse(Console.ReadLine(), out ya);
+
+Console.WriteLine("Введите Z1: ");
+int za = 0;
+int.TryParse(Console.ReadLine(), out za);
+
+Console.WriteLine("Введите Х2: ");
+int xb = 0;
+int.TryParse(Console.ReadLine(), out xb);
+
+Console.WriteLine("Введите Y2:");
+int yb = 0;
+int.TryParse(Console.ReadLine(), out yb);
+
+Console.WriteLine("Введите Z2: ");
+int zb = 0;
+int.TryParse(Console.ReadLine(), out zb);
+
+Console.WriteLine($"Растояние: {Math.Sqrt(Math.Pow(xa - xb, 2) + Math.Pow(ya - yb, 2) + Math.Pow(za - zb, 2))}");
