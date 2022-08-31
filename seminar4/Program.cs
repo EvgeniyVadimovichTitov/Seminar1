a61c797c8c05926cecd3b3b408289ca7f5105530
EvgeniyVadimovichTitov <jekon777999@gmail.com>
2022-08-31 19:44:43 +0300

    Complete Task1

--- seminar4/Program.cs
+++ seminar4/Program.cs
@@ -1,3 +1,24 @@
 ﻿//Seminar4Task1 - Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-//Seminar4Task2 - Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
-//Seminar4Task3 - Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+Console.WriteLine("Введите число:");
+int number = 0;
+int.TryParse(Console.ReadLine(), out number);
+
+Console.WriteLine("Введите степень:");
+int degree = 0;
+int.TryParse(Console.ReadLine(), out degree);
+
+int Method(int a, int b)
+{
+    int n = a;
+    int i = 2;
+    if (i <= b)
+    {
+        n *= a;
+        i++;
+        return n;
+    }
+    if (b == 0) return 1;
+    else return n;
+}
+
+Console.WriteLine($"{number} в степени {degree} равно: {Method(number, degree)}");
