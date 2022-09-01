59178e6791810fbc134e8ba83e0f4371525bf86a
EvgeniyVadimovichTitov <jekon777999@gmail.com>
2022-09-01 12:08:19 +0300

    Complete task2

--- seminar4/Program.cs
+++ seminar4/Program.cs
@@ -1,3 +1,26 @@
-﻿//Seminar4Task1 - Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-//Seminar4Task2 - Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
-//Seminar4Task3 - Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+﻿//Seminar4Task2 - Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.Write("Введите число:");
+string number = Console.ReadLine();
+int[] massiv = new int[number.Length];
+int ConvertNumber = int.Parse(number);
+
+void CreateMassiv(int[] j, int n)
+{
+    int i = j.Length - 1;
+    while (i >= 0)
+    {
+        int t = n % 10;
+        n /= 10;
+        j[i] = t;
+        i--;
+    }
+}
+
+int SumMassiv(int[] f)
+{
+    int sum = f[0];
+    for (int q = 1; q < f.Length; sum = sum + f[q], q++) ;
+    return sum;
+}
+CreateMassiv(massiv, ConvertNumber);
+Console.WriteLine($"Сумма цифр числа равна: {SumMassiv(massiv)}");
