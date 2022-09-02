dc66910ec69b4def355d485be48afebb9d50acc0
EvgeniyVadimovichTitov <jekon777999@gmail.com>
2022-09-02 17:38:57 +0300

    Complete Task3

--- seminar5/Program.cs
+++ seminar5/Program.cs
@@ -1,17 +1,33 @@
-﻿/* Seminar5Task1 - Задайте массив заполненный случайными положительными трёхзначными числами. 
-Напишите программу, которая покажет количество чётных чисел в массиве.
-
-[345, 897, 568, 234] -> 2
-
-Seminar5Task2 - Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, 
-стоящих на нечётных позициях.
-
-[3, 7, 23, 12] -> 19
-
-[-4, -6, 89, 6] -> 0
-
-Seminar5Task2 - Задайте массив вещественных чисел. Найдите разницу между максимальным и 
+﻿/* Seminar5Task2 - Задайте массив вещественных чисел. Найдите разницу между максимальным и 
 минимальным элементов массива.
 
 [3 7 22 2 78] -> 76
 */
+
+float[] array = new float[new Random().Next(1, 11)];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-10, 11);
+    Console.Write($"{array[i]} ");
+}
+float FoundMax(float[] massiv)
+{
+    float max = massiv[0];
+    for (int j = 1; j < massiv.Length; j++)
+    {
+        if (massiv[j] > max) max = massiv[j];
+    }
+    return max;
+}
+float FoundMin(float[] massiv)
+{
+    float min = massiv[0];
+    for (int j = 1; j < massiv.Length; j++)
+    {
+        if (massiv[j] < min) min = massiv[j];
+    }
+    return min;
+}
+
+Console.Write($"\n Разница между максимальным и минимальным элементами массива равна: {FoundMax(array) - FoundMin(array)}");
