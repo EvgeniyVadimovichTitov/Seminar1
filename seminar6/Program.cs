857573d6896dd811468652b96e005c43e09447fa
EvgeniyVadimovichTitov <jekon777999@gmail.com>
2022-09-06 17:10:29 +0300

    Complete task2

--- seminar6/Program.cs
+++ seminar6/Program.cs
@@ -3,15 +3,15 @@
 b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
 
 Console.Write("Введите k1  и b1 для y = k1 * x + b1:");
-int k1 = int.Parse(Console.ReadLine()!);
-int b1 = int.Parse(Console.ReadLine()!);
+double k1 = double.Parse(Console.ReadLine()!);
+double b1 = double.Parse(Console.ReadLine()!);
 
 
 Console.Write("Введите k2 и b2 для y = k2 * x + b2:");
-int k2 = int.Parse(Console.ReadLine()!);
-int b2 = int.Parse(Console.ReadLine()!);
+double k2 = double.Parse(Console.ReadLine()!);
+double b2 = double.Parse(Console.ReadLine()!);
 
-double FoundPerX(int a, int b, int c, int d)
+double FoundPerX(double a, double b, double c, double d)
 {
     double x = 0;
     return x = (d - b) / (a - c);
@@ -19,4 +19,4 @@ double FoundPerX(int a, int b, int c, int d)
 
 if (k1 == k2 && b1 != b2) Console.WriteLine("Прямые параллельны пересечений нет");
 else if (k1 == k2 && b1 == b2) Console.WriteLine("Прямые наложены пересечений безконечное множество");
-else Console.Write($"координата пересечения: ({FoundPerX(k1, b1, k2, b2)}, {k1 * FoundPerX(k1, b1, k2, b2) + b2})");
+else Console.Write($"координата пересечения: ({FoundPerX(k1, b1, k2, b2)}, {k2 * FoundPerX(k1, b1, k2, b2) + b2})");
