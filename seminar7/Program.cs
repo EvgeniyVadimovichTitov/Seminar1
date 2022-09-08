66527c0ff0b7d443d75e754e09d3507f0b867c2b
EvgeniyVadimovichTitov <jekon777999@gmail.com>
2022-09-08 22:41:35 +0300

    Complete Task 2

--- seminar7/Program.cs
+++ seminar7/Program.cs
@@ -6,3 +6,37 @@
 8 4 2 4
 17->такого числа в массиве нет*/
 
+Console.Write("Введите количество строк и солбцов: ");
+int m = int.Parse(Console.ReadLine()!);
+int n = int.Parse(Console.ReadLine()!);
+
+int[,] array = new int[m, n];
+Console.WriteLine();
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(10);
+        Console.Write($" {array[i, j]} ");
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Введите идентефикаторы искомого элемента (строка и столбец): ");
+int numbString = int.Parse(Console.ReadLine()!);
+int numbStolb = int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+
+if (numbString > array.GetLength(0) || numbStolb > array.GetLength(1))
+    Console.Write("Такого элемента нет");
+else for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i == numbString && j == numbStolb)
+                Console.Write(array[i, j]);
+        }
+    }
+
+
