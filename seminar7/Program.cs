1177e20b3a64f9cfa1ff52e13cf479fb85984660
EvgeniyVadimovichTitov <jekon777999@gmail.com>
2022-09-08 22:22:44 +0300

    1

--- seminar7/Program.cs
+++ seminar7/Program.cs
@@ -8,13 +8,15 @@ int m = int.Parse(Console.ReadLine()!);
 int n = int.Parse(Console.ReadLine()!);
 
 double[,] array = new double[m, n];
+Console.WriteLine();
+
 
 for (int i = 0; i < array.GetLength(0); i++)
 {
     for (int j = 0; j < array.GetLength(1); j++)
     {
-        array[i, j] = new Random.Next(-9.9, 9.9);
-        Console.Write(array[i, j]);
+        array[i, j] = double.Parse(new Random().Next(-9.9, 10.9));
+        Console.Write($" {array[i, j]} ");
     }
-    Console.WriteLine()
+    Console.WriteLine();
 }
